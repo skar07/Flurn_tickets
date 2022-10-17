cc39150677a2675b2418ba451c05d1ba46bb3f30
skar07 <sanskarpandey0@gmail.com>
2022-10-17 18:36:18 +0530

    feat: implement server

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.use(
         extended: true,
     })
 )
+const seatRoute = require('./routes/seats')
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API'})
+})
+app.use('api/v1/seats', seatRoute)
+
 const PORT = process.env.PORT || 3000
 
-app.listen(PORN, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
