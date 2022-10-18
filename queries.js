59db1bb54991cca091fe097151f7af368e74617f
skar07 <sanskarpandey0@gmail.com>
2022-10-18 15:59:30 +0530

    chore: initialize DB connection client

--- queries.js
+++ queries.js
@@ -1,10 +1,19 @@
-const Pool = require('pg').Pool
+require('dotenv').config()
+const { Pool, Client } = require('pg')
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DB,
-  password: process.env.PASS,
-  port: process.env.DB_PORT,
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASS,
+    port: process.env.DB_PORT,
 })
 
-module.exports = pool;
+const client = new Client({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASS,
+    port: process.env.DB_PORT,
+})
+
+module.exports = { pool, client };
