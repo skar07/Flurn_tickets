e1eacd8913955e1c5f14942d62a458793f209a1b
skar07 <sanskarpandey0@gmail.com>
2022-10-18 15:58:45 +0530

    feat: implement DB connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const { Client } = require('pg')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
@@ -10,12 +11,27 @@ app.use(
 )
 const seatRoute = require('./routes/seats')
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API'})
+    response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 app.use('api/v1/seats', seatRoute)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+const start = async () => {
+    const client = new Client({
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DB,
+        password: process.env.PASS,
+        port: process.env.DB_PORT,
+    })
+    try {
+        await client.connect()
+        app.listen(PORT, () => {
+            console.log(`Listening on port: ${PORT}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+start();
